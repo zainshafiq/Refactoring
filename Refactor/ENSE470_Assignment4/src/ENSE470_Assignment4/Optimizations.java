8c5d077effd01c51c68ce653663373459c114a82
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-04-04 23:54:41 -0600

    Commit

---
+++ Refactor/ENSE470_Assignment4/src/ENSE470_Assignment4/Optimizations.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package optimizations;
+
+import java.util.Random;
+
+/**
+ *
+ * @author luigibenedicenti
+ *
+ * @modified kcyow March 14th 2019
+ *
+ */
+
+public class Optimizations {
+/*
+-----------------------------------------------------------------
+static long tBeg, tEnd;
+-----------------------------------------------------------------
+Refactor #3 : Rename Variable
+I renamed tBeg and tEnd as timeBeginning and timeEnd for a better meaning
+*/
+
+static long timeBeginning, timeEnd;
+static int testSize = 1000;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    /*
+    ----------------------------------------------------------------------------------------------------
+        System.out.println("Optimization example program\n\n");
+        StrengthReduction test1 = new StrengthReduction();
+        tBeg = System.nanoTime();
+        test1.recursiveSum(testSize);
+        tEnd = System.nanoTime();
+        System.out.println("Strength reduction\n Recursive time:" + (tEnd - tBeg));
