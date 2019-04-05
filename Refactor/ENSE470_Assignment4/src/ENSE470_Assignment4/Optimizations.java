8ac3339e02a2720dfeb022c2602db8ddcfce6dd8
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-04-04 23:55:26 -0600

    Commit

--- Refactor/ENSE470_Assignment4/src/ENSE470_Assignment4/Optimizations.java
+++ Refactor/ENSE470_Assignment4/src/ENSE470_Assignment4/Optimizations.java
@@ -1,3 +1,7 @@
+//---------- Optimizations.java ----------
+
+package ENSE470_Assignment4;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
