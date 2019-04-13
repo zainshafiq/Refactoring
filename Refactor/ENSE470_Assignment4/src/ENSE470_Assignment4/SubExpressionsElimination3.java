09f98fc74911ec96f8bf0a325486b1fa6a5ea3d4
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-04-12 19:31:05 -0600

    Commit

---
+++ Refactor/ENSE470_Assignment4/src/ENSE470_Assignment4/SubExpressionsElimination3.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package optimizations;
+
+import java.util.Random;
+
+/**
+ *
+ * @author luigibenedicenti
+ *
+ * @modified kcyow March 14th 2019
+ *
+ */
+public class SubExpressionsElimination3 {
+
+    Random gen;
+    double d, lim, max, sx, sy;
+
+    public void exampleNoElimination() {
+        double x = d * (lim / max) * sx;
+        double y = d * (lim / max) * sy;
+    }
+
+    public void exampleElimination() {
+        double se = d * (lim / max);
+        double x = se * sx;
+        double y = se * sy;
+    }
+
+    public void SubExpressionsElimination() {
+        gen = new Random();
+        d = gen.nextDouble();
+        lim = gen.nextDouble();
+        max = gen.nextDouble();
+        sx = gen.nextDouble();
+        sy = gen.nextDouble();
+    }
+}
